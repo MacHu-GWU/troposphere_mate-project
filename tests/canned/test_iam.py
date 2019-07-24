6ec59af4757bb896c6fe6ddec5e8bea2cf317b73
Sanhe Hu <husanhe@gmail.com>
2019-07-24 18:03:43 -0400

    test canned template

---
+++ tests/canned/test_iam.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+from pytest import raises, approx
+from troposphere_mate.canned import iam
+
+
+def test():
+    canned_iam_role = iam.CannedCommonLambdaFunctionIamRole(
+        PROJECT_NAME="my_project",
+        STAGE="dev",
+    )
+    canned_iam_role.create_template()
+    canned_iam_role.template.to_json()
+    canned_iam_role.iam_role_lbd_basic_exec.to_json()
+    canned_iam_role.iam_role_lbd_s3_full_access.to_json()
+
+
+if __name__ == "__main__":
+    import os
+
+    basename = os.path.basename(__file__)
+    pytest.main([basename, "-s", "--tb=native"])
---
+++ tests/canned/test_vpc.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+from pytest import raises, approx
+from troposphere_mate.canned import vpc
+
+
+def test():
+    can = vpc.VPCTier(
+        PROJECT_NAME="my_project",
+        STAGE="dev",
+        VPC_CIDR_ID=192,
+        N_PUBLIC_SUBNET=2,
+        N_PRIVATE_SUBNET=2,
+        USE_NAT_GW_PER_PRIVATE_SUBNET_FLAG=True,
+    )
+    can.pprint()
+    tpl = can.create_template()
+
+    assert can.vpc is not None
+    assert can.public_subnet_list is not None
+    assert can.private_subnet_list is not None
+    assert can.subnet_list is not None
+    assert can.igw is not None
+    assert can.eip_list is not None
+    assert can.ngw_list is not None
+    assert can.public_route_table is not None
+    assert can.sg_for_ssh is not None
+
+
+if __name__ == "__main__":
+    import os
+
+    basename = os.path.basename(__file__)
+    pytest.main([basename, "-s", "--tb=native"])
